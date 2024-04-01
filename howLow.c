c03a4fff7e57c8a9c4a43c0ea3d8c4cf2f88bcab
Izaac Haro <162258152+ibenjaminh@users.noreply.github.com>
2024-03-31 17:27:54 -0700

    Create howLow.c
    Write a program that prints out like this, notice exactly how it is printing ok:
    how low low low low low 
    can you go go go go go 
    You are going to use a for loop on the first line to print the low word 5 times and then use a while loop on the second line the go word 5 times.

---
+++ howLow.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+printf("how");
+
+     for (int i = 0; i < 5; i++)
+          printf(" low");
+
+printf("\ncan you ");
+
+     int i = 0;
+     while (i < 5) {
+          printf("go ");
+          i++;
+     }
+}
