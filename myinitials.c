9d1b89e23ba1125d879532ac12c88b76fd07c3a5
Izaac Haro <162258152+ibenjaminh@users.noreply.github.com>
2024-03-31 17:24:24 -0700

    Create myinitials.c

---
+++ myinitials.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+     printf("***********  *          *\n    @        @          @\n    $        $          $\n    *        *          *\n    @        @@@@@@@@@@@@\n    $        $          $\n    *        *          *\n    @        @          @\n$$$$$$$$$$$  $          $");
+     printf("\n||||\t\t||||\n\n\t\t^\n________________");
+
+     return 0;
+}
