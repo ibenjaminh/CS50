6e21b1f2dcc119c13414d6d645614d256c0b20f6
Izaac Haro <162258152+ibenjaminh@users.noreply.github.com>
2024-03-31 17:23:33 -0700

    Create tictacshmoe.c

---
+++ tictacshmoe.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("1 1 1\n0 0 0\n0 0 0");
+    printf("\n\n0  0  1\n0  1  0\n1  0  0");
+    printf("\n\n1\t0\t0\n\n0\t1\t0\n\n0\t0\t1");
+
+     return 0;
+}
